0f38b3d9e4513aceba926608334e47179fd85f22
Evgeniy <yndrab@yandex.ru>
2024-08-12 11:22:49 +0400

    1

--- src/test/java/ru/cbgr/qa/stress/mvd/step/prepare/LoginStep.java
+++ src/test/java/ru/cbgr/qa/stress/mvd/step/prepare/LoginStep.java
@@ -13,51 +13,51 @@ public class LoginStep {
 
             pause(ofMillis(100), ofMillis(100))
                     .exec(http("Логин")
-                            .post("/Login/Login").asJson()
-                            .body(ElFileBody("json/prepare/sendLoginZUP.json")).asJson()
+                            .post("/account/login").asJson()
+                            .body(ElFileBody("json/prepare/login.json")).asJson()
                             .check(status().is(200))
-                    )
+                    );
 
-                    .exec(http("Логин - установить роль")
-                            .post("/Login/Roles").asJson()
-                            .body(ElFileBody("json/prepare/setUserRoleZUP.json")).asJson()
-                            .check(status().is(200))
-                            .check(headerRegex("Set-Cookie", "SessionID=(.*?);").find(1).saveAs("sessionId"))
-                    )
-
-                    .exec(http("PreLogin")
-                            .post("/api/xcom/userauth/preLogin").asJson()
-                            .check(status().is(200)))
-                    .pause(ofSeconds(2))
-
-                    .exec(http("GetPostLoginForm")
-                            .post("/api/xcom/view/getPostLoginForm").asJson()
-                            .header("Sessionid", "#{sessionId}")
-                            .check(status().is(200)))
-                    .pause(ofSeconds(2))
-
-                    .exec(http("Роль - getUserRole")
-                            .post("/api/xcom/view/getUserRole").asJson()
-                            .header("Sessionid", "#{sessionId}")
-                            .check(status().is(200)))
-                    .pause(ofSeconds(2))
-
-                    .exec(http("Меню - getMenu")
-                            .post("/api/xcom/view/getMenu").asJson()
-                            .header("Sessionid", "#{sessionId}")
-                            .check(status().is(200)))
-                    .pause(ofSeconds(2))
-
-                    .exec(http("Тулбар - getToolbar")
-                            .post("/api/xcom/view/getToolbar").asJson()
-                            .header("Sessionid", "#{sessionId}")
-                            .check(status().is(200)))
-                    .pause(ofSeconds(2))
-
-                    .exec(http("Состояние приложения - getAppState")
-                            .post("/api/xcom/view/getAppState").asJson()
-                            .header("Sessionid", "#{sessionId}")
-                            .check(status().is(200))
-                            .check(jsonPath("$.result[1].resultAction.formId").saveAs("formId")))
-                    .pause(ofSeconds(2));
+//                    .exec(http("Логин - установить роль")
+//                            .post("/Login/Roles").asJson()
+//                            .body(ElFileBody("json/prepare/setUserRoleZUP.json")).asJson()
+//                            .check(status().is(200))
+//                            .check(headerRegex("Set-Cookie", "SessionID=(.*?);").find(1).saveAs("sessionId"))
+//                    )
+//
+//                    .exec(http("PreLogin")
+//                            .post("/api/xcom/userauth/preLogin").asJson()
+//                            .check(status().is(200)))
+//                    .pause(ofSeconds(2))
+//
+//                    .exec(http("GetPostLoginForm")
+//                            .post("/api/xcom/view/getPostLoginForm").asJson()
+//                            .header("Sessionid", "#{sessionId}")
+//                            .check(status().is(200)))
+//                    .pause(ofSeconds(2))
+//
+//                    .exec(http("Роль - getUserRole")
+//                            .post("/api/xcom/view/getUserRole").asJson()
+//                            .header("Sessionid", "#{sessionId}")
+//                            .check(status().is(200)))
+//                    .pause(ofSeconds(2))
+//
+//                    .exec(http("Меню - getMenu")
+//                            .post("/api/xcom/view/getMenu").asJson()
+//                            .header("Sessionid", "#{sessionId}")
+//                            .check(status().is(200)))
+//                    .pause(ofSeconds(2))
+//
+//                    .exec(http("Тулбар - getToolbar")
+//                            .post("/api/xcom/view/getToolbar").asJson()
+//                            .header("Sessionid", "#{sessionId}")
+//                            .check(status().is(200)))
+//                    .pause(ofSeconds(2))
+//
+//                    .exec(http("Состояние приложения - getAppState")
+//                            .post("/api/xcom/view/getAppState").asJson()
+//                            .header("Sessionid", "#{sessionId}")
+//                            .check(status().is(200))
+//                            .check(jsonPath("$.result[1].resultAction.formId").saveAs("formId")))
+//                    .pause(ofSeconds(2));
 }
