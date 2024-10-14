8c0f6fc668c31ede6d17c7ec6ee99798c758dafc
Evgeniy <yndrab@yandex.ru>
2024-10-14 17:59:06 +0400

    1

--- src/test/java/ru/cbgr/qa/stress/mvd/step/prepare/LoginStep.java
+++ src/test/java/ru/cbgr/qa/stress/mvd/step/prepare/LoginStep.java
@@ -16,31 +16,33 @@ public class LoginStep {
                             .post("/account/login").asJson()
                             .body(ElFileBody("json/prepare/login.json")).asJson()
                             .check(status().is(200))
-                    );
+                            .check(headerRegex("Set-Cookie", "SessionID=(.*?);").find(1).saveAs("sessionId"))
+                    )
 
-//                    .exec(http("Логин - установить роль")
-//                            .post("/Login/Roles").asJson()
-//                            .body(ElFileBody("json/prepare/setUserRoleZUP.json")).asJson()
-//                            .check(status().is(200))
+                    .exec(http("Логин - установить роль")
+                            .post("/log?contextOrganizationId=57").asJson()
+                            .body(ElFileBody("json/prepare/Log.json")).asJson()
+                            .check(status().is(200)))
+                            .pause(ofSeconds(2))
 //                            .check(headerRegex("Set-Cookie", "SessionID=(.*?);").find(1).saveAs("sessionId"))
 //                    )
 //
-//                    .exec(http("PreLogin")
-//                            .post("/api/xcom/userauth/preLogin").asJson()
-//                            .check(status().is(200)))
-//                    .pause(ofSeconds(2))
-//
-//                    .exec(http("GetPostLoginForm")
-//                            .post("/api/xcom/view/getPostLoginForm").asJson()
+                    .exec(http("PreLogin")
+                            .get("/notification/personal/all?contextOrganizationId=57&accountId=359&page=0&size=20").asJson()
+                            .check(status().is(200)))
+                    .pause(ofSeconds(2))
+
+                    .exec(http("GetPostLoginForm")
+                            .get("/notification/personal/unread?contextOrganizationId=57&accountId=359").asJson()
 //                            .header("Sessionid", "#{sessionId}")
-//                            .check(status().is(200)))
-//                    .pause(ofSeconds(2))
+                            .check(status().is(200)))
+                    .pause(ofSeconds(2));
 //
 //                    .exec(http("Роль - getUserRole")
 //                            .post("/api/xcom/view/getUserRole").asJson()
 //                            .header("Sessionid", "#{sessionId}")
 //                            .check(status().is(200)))
-//                    .pause(ofSeconds(2))
+//                    .pause(ofSeconds(2));
 //
 //                    .exec(http("Меню - getMenu")
 //                            .post("/api/xcom/view/getMenu").asJson()
